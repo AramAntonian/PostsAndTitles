e30fcecfa60b363dc0703c52f96a2f5dc7b10474
AramAntonian <112649701+AramAntonian@users.noreply.github.com>
2022-11-30 19:27:03 +0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
-import { useEffect, useMemo, useRef, useState } from "react"
+import {  useEffect, useMemo, useRef, useState } from "react"
 import './style.css'
 
 function Header({userId,setUserId}){
     const inputRef = useRef()
     const [data,setData] = useState([])
+    const buttonRef = useRef()
+
     
     useEffect(()=>{
         fetch("https://jsonplaceholder.typicode.com/users").then(rep => rep.json()).then(repo => setData(repo))
@@ -22,7 +24,8 @@ function Header({userId,setUserId}){
             alert("invalid email!")
             
         }
-        inputRef.current.value = ""     
+        inputRef.current.value = "" 
+        inputRef.current.focus()    
     }
     const userName = useMemo(()=>{
         if(userId !== ""){
@@ -32,16 +35,34 @@ function Header({userId,setUserId}){
     }
     ,[userId]
     )
+    const hint= useMemo(()=>{
+        if(data.length){
+            return [data[0].email,data[1].email]
+        }
+        else return ""
+    }
+    ,[data]
+    )
+
 
     return (
+        <div className = "cont">
         <header >
             <div className="input">
-                <input type = "text" ref = {inputRef}/>
-                <button onClick = {handleLogin}>login</button>
+                <input type = "text" ref = {inputRef} onKeyDown = {(e)=>{
+                    if(e.keyCode === 13)
+                    buttonRef.current.focus()
+                }}/>
+                <button onClick = {handleLogin} ref = {buttonRef}>login</button>
             </div>
     
             <div className = "userName">{userName} </div>
         </header>
+        <footer className="hint">
+            {hint[0]}<br />
+            {hint[1]}
+        </footer>
+        </div>
     )
 }
 
