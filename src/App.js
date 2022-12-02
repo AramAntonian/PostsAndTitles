f9438f114c1bac8968aaceac7f9c63cb983f553d
AramAntonian <112649701+AramAntonian@users.noreply.github.com>
2022-12-02 20:10:51 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PostAndTitles from './context/PostAndTitles.js'
 import GetPosts from './components/GetPosts.js'
 import GetTitles from './components/GetTitles.js'
 import {useState,useEffect} from "react"
+import {v4 as uuidv4} from "uuid"
  
 function App() {
   const [userId,setUserId] = useState("")
@@ -11,16 +12,36 @@ function App() {
   
   useEffect(()=>{
     if(userId !== "")
-      fetch(`https://jsonplaceholder.typicode.com/posts`).then(rep => rep.json()).then(repo => setPosts(repo.filter(el => el.userId === userId)))
+      fetch(`https://jsonplaceholder.typicode.com/posts`).then(rep => rep.json())
+      .then(repo => setPosts(repo.filter(el =>{
+        el.key = uuidv4()
+        el.handleShow = handleShow
+        el.letShow = {
+          display: "none",
+        }
+        return el.userId === userId
+      })))
+  
+        function handleShow(event){
+          console.log(event.target.id)
+          setPosts(data => data.map((el)=>{
+              if(el.key === event.target.id)
+                  el.letShow = {
+                      display:"block",
+                  }
+              return el
+          }))
+      }
+
       
     
   },[userId])
-  
+  console.log(posts)
   return (
     <>
       <Header userId = {userId} setUserId = {setUserId} />
       <PostAndTitles.Provider value = {posts}>
-        <GetTitles />
+        <GetTitles  />
         <GetPosts />
       </PostAndTitles.Provider>
     </>
